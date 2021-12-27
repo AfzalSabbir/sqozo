99488ca942a79636579707020f3a999975b6a841
Tarikul Hasan Shuvo <tarikul15-7944@diu.edu.bd>
2021-12-28 00:56:59 +0600

    v2 intil

---
+++ v2/contact.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
+    <title>Contact</title>
+</head>
+<body>
+<div id="contact" class="">
+    <div class="nav-container">
+        <nav class="navbar container navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img
+                            src="https://theme.zdassets.com/theme_assets/11061851/a8d8592eee3906aa861ec80259bfdf1b1487eddd.png"
+                            width="120" height="45" alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-danger fw-light" href="#" id="navbarDropdown"
+                               role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                English (US)
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">العربية</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <button class="btn btn-danger" type="submit"><i class="fa fa-plus-circle me-2"></i>PLACE YOUR AD
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="main-container">
+        <div class="container mb-2">
+            <div class="text-center pt-5">
+                <h1 class="text-secondary">How can we help you?</h1>
+                <div class="input-group search">
+                    <input class="form-control" type="search" placeholder="Search our Articles" aria-label="Search">
+                    <button class="btn search-btn"><i class="fa fa-search"></i></button>
+                </div>
+                <div class="mx-auto mt-3">
+                    <img src="https://theme.zdassets.com/theme_assets/11061851/422ed97bdc29bdf9a1f70ea68acd697345ba52e2.jpg"
+                         width="37%" alt="">
+                </div>
+
+            </div>
+        </div>
+        <hr>
+        <div class="main-content">
+            <div class="container">
+                <div class="btn-group" role="group" aria-label="Basic outlined example">
+                    <a href="" class="btn help-btn"><i class="fa fa-angle-left me-2"></i>Help Center</a>
+                    <a href="" class="btn fw-bold">Submit a request</a>
+                </div>
+                <h2>Help us help you faster!</h2>
+                <hr>
+                <div class="row pt-4">
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="card-body p-4">
+                                <p class="pb-4">Accurately selecting your specific issue from the drop-down lists below
+                                    will enable
+                                    us to direct your message to the right deparment. Once you select your issue, you
+                                    will be able to contact us.</p>
+
+                                <form>
+                                    <div class="mb-3">
+                                        <label class="form-label">How can we help? Select a form that best meets your
+                                            needs:</label>
+                                        <select class="form-select" aria-label="Default select example">
+                                            <option selected>Contact Form English</option>
+                                            <option value="1">One</option>
+                                            <option value="2">Two</option>
+                                            <option value="3">Three</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="email" class="form-label">Your email address<span
+                                                    class="text-danger">*</span></label>
+                                        <input type="email" class="form-control" id="email">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="subject" class="form-label">Subject<span
+                                                    class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" id="subject">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="description" class="form-label">Description<span
+                                                    class="text-danger">*</span></label>
+                                        <textarea class="form-control" id="description" rows="4"></textarea>
+                                        <div class="form-text my-3 opacity-50">Please enter the details of your request.
+                                            A member of our support staff will respond as soon as possible.
+                                        </div>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="form-label">What is your inquiry about?</label>
+                                        <select class="form-select" aria-label="Default select example">
+                                            <option selected>-</option>
+                                            <option value="1">One</option>
+                                            <option value="2">Two</option>
+                                            <option value="3">Three</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="subject" class="form-label">Attachments</label>
+                                        <br>
+                                        <label type="button" for="attachments" class="file-upload form-label">
+                                            <img src="./asessts/icons/camera.png" alt="">
+                                            <small class="text-danger">Add file</small> or drop files here</label>
+                                        <input hidden type="file" class="form-control" id="attachments">
+                                    </div>
+                                    <button type="submit" class="btn btn-danger w-100 mt-3 btn-lg rounded-0">Submit
+                                    </button>
+                                    <div class="form-text my-3"><span
+                                                class="text-danger">*</span> Mandatory fields
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card position-relative">
+                            <div class="card-header">
+                                <h5>Trending articles</h5>
+                            </div>
+                            <i class="fa fa-sort-down"></i>
+                            <div class="card-body list pt-5">
+                                <ul>
+                                    <li><a href="">4 Things you need to know about VAT</a></li>
+                                    <li><a href="">What is VAT and what how does it affect my relationship with dubizzle?</a></li>
+                                    <li><a href="">How can I change my mobile number?</a></li>
+                                    <li><a href="">Scam alert: do not share your credit card information!</a></li>
+                                    <li><a href="">Phishing through chat</a></li>
+                                    <li><a href="">What is SMS Phishing?</a></li>
+                                    <li><a href="">Real examples of fraud</a></li>
+                                    <li><a href="">Phishing Attempt: is this an email that I received from dubizzle?</a></li>
+                                    <li><a href="">How to Verify Your dubizzle Account using the dubizzle verification code?</a></li>
+                                    <li><a href="">Featured Ads FAQs</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-image"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-6 my-auto">
+                    <div class="link d-flex">
+                        <a href="" class="ps-0">Home</a>
+                        <a href="">Terms of Use</a>
+                        <a href="">Privacy Policy</a>
+                        <a href="">About Us</a>
+                        <a href="" class="border-0">Place Your Ad</a>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="d-flex justify-content-end">
+                        <img src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+                             width="30%" height="65px" alt="">
+                        <img class="mt-1"
+                             src="https://www.pngitem.com/pimgs/m/394-3943103_app-store-badge-png-available-on-the-app.png"
+                             width="30%" height="56px" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copy-right">
+            <div class="container">
+                <span>© sqozo.com 2021, All Rights Reserved.</span>
+            </div>
+        </div>
+    </footer>
+    <div class="chat-btn">
+        <button class="btn btn-danger rounded-pill p-2 px-3" type="submit"><i class="far fa-comment me-2"></i>Chat
+        </button>
+    </div>
+</div>
+<script src="./plugins/bootstrap/bootstrap.js"></script>
+</body>
+</html>
